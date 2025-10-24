789ec92239d29aab3b36aa29639a6cf6ff297ac1
RoseaneNunes <roseane.anjos@aluno.uepb.edu.br>
2025-10-24 16:08:27 -0300

    Feat: criar Hero section da Pagina materiais

---
+++ FrontEnd/src/components/Sections/MaterialsSection/MaterialsSection.tsx
@@ -0,0 +1,27 @@
+import { IoArrowDownSharp } from 'react-icons/io5'
+import background from '../../../assets/images/backgrounds/sections/Materials.png'
+import learnMoreIcon from "@/assets/images/icons/learn-more.png"
+
+export default function MaterialsSection(){
+    return(
+        <section>
+            <div className="relative w-full h-[870px]  bg-cover bg-center  max-md:h-[745px]  text-white flex justify-evenly items-center pt-[280px] "  style={{ backgroundImage: `url(${background})`}}>
+                 <div className="w-[78%]">
+        <div className="">
+          <h1 className="text-[48px] font-bold  max-w-[477px] max-sm:max-w-[318px] max-sm:text-[32px] max-sm:text-start">
+           Materiais Gratuitos
+          </h1>
+          <div className="max-lg:hidden max-lg:absolute min-w-[80vw]"></div>
+          <h2 className="text-[24px] font-normal  max-w-[522px] pt-10 text-subtext w-[100%] max-sm:max-w-[275px] max-sm:text-[16px] max-md:text-start">
+           Acreditamos que o conhecimento é a chave para uma internacionalização bem-sucedida. Por isso, nossa empresa tem o prazer de oferecer um vasto acervo de materiais gratuitos desenvolvidos por nossos especialistas.
+          </h2>
+        </div>
+        <div className="mt-[20px] h-[300px] flex justify-center relative">
+          <img className="w-[300px] rotating" src={learnMoreIcon} alt="Saiba mais"/>
+          <IoArrowDownSharp className="text-[50px] text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+        </div>
+      </div>
+            </div>
+        </section>
+    )
+}
