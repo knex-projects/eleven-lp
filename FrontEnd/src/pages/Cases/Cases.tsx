3310d81e81c1335b4edbb847ae2c81338d0b1d53
devedsonalves <devedsonalves@gmail.com>
2025-10-27 11:44:38 -0300

    chore: remove unnecessary import

--- FrontEnd/src/pages/Cases/Cases.tsx
+++ FrontEnd/src/pages/Cases/Cases.tsx
@@ -2,13 +2,12 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 import CasesSection from "@/components/Sections/CasesSection/CasesSection";
-import MaterialsSection from "@/components/Sections/MaterialsSection";
 
 export default function Cases() {
   return (
     <>
       <Header page="cases" />
-      <CasesSection/>
+      <CasesSection />
       <Footer />
     </>
   );
