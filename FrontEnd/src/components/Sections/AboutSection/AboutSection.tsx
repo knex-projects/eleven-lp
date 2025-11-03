63f927524b3fbcf2c19a29b3f0854d8852658066
RoseaneNunes <roseane.anjos@aluno.uepb.edu.br>
2025-11-03 18:11:44 -0300

    style: ajustar posicionamento  e tamanhos de titulos e subtitulos  e  ajustar gradiente do background remove: remover o video feat: adionar icone circular

--- FrontEnd/src/components/Sections/AboutSection/AboutSection.tsx
+++ FrontEnd/src/components/Sections/AboutSection/AboutSection.tsx
@@ -1,37 +1,31 @@
 import backgroundImage from "@/assets/images/backgrounds/sections/about-us.png";
+import { IoArrowDownSharp } from "react-icons/io5";
+import learnMoreIcon from "@/assets/images/icons/learn-more.png"
 
 export default function AboutSection() {
   return (
 	<section
-        className="w-screen h-[870px] bg-cover flex gap-28 max-xl:gap-20 items-center max-lg:flex-col justify-center pt-20 text-primary-text bg-center max-md:pt-0"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
+        className="w-screen h-[870px] bg-cover  flex flex-col items-center max-lg:flex-col justify-center pt-20 text-primary-text bg-center max-md:pt-0 max-sm:h-[745px]"
+        style={{
+    backgroundImage: `linear-gradient(to right, rgba(19, 24, 69, 1), rgba(0,0,0,0.2)), url(${backgroundImage})`,
+  }}
     >
-        <div className="space-y-6">
-          <h1 className="text-[45px] font-bold  max-w-[493px] w-[86vw] max-lg:text-[40px]">
+        <div className="w-[88%]  relative max-sm:pt-[120px] max-2xl:max-w-[1150px] min-2xl:w-[70%]">
+          <h1 className="text-[48px] font-bold max-w-[572px]  w-[100%] max-lg:text-[40px] max-sm:text-[32px] ">
             Prazer, somos a Eleven Consultoria!
           </h1>
-          <iframe
-            className="ml-auto mr-auto min-lg:hidden min-lg:absolute max-w-[389px] w-[86vw]"
-            height="209"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-            title="YouTube video"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-          />
-          <h2 className="text-[24px] font-normal  max-w-[481px] pt-6 text-subtext w-[88vw] max-lg:text-[20px] text-justify">
+          <h2 className="text-[24px] font-normal  max-w-[481px] pt-6 text-subtext w-[100%] max-lg:text-[20px] text-justify max-sm:text-[16px] max-sm:max-w-[299px]">
             A Eleven oferece consultoria estratégica personalizada para expandir
             sua empresa no mercado internacional e fortalecer sua presença
             global.
           </h2>
         </div>
+         <div className="mt-[20px] h-[300px] flex justify-center relative min-xl:hidden">
+          <img className="w-[300px] rotating" src={learnMoreIcon} alt="Saiba mais"/>
+          <IoArrowDownSharp className="text-[50px] text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+        </div>
 
-        <iframe
-          className="max-lg:hidden max-lg:absolute w-[552px] h-[294px] max-xl:w-[452px]"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          title="YouTube video"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        />
+       
     </section>
   )
 }
