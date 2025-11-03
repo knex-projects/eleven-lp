948d950cbad27536a70777d8a4dd7c0cb7cebc46
RoseaneNunes <roseane.anjos@aluno.uepb.edu.br>
2025-11-03 18:16:02 -0300

    style: ajustar posicionamento  e tamanhos de titulos, textos e divs para telas menores

--- FrontEnd/src/components/Sections/HistorySection/HistorySection.tsx
+++ FrontEnd/src/components/Sections/HistorySection/HistorySection.tsx
@@ -2,19 +2,19 @@ import logoImg from "@/assets/images/logos/eleven-big-logo.svg";
 
 export default function HistorySection() {
   return (
-    <section className="w-screen p-15 bg-background flex items-center min-md:justify-evenly max-xl:flex-col-reverse lg:flex-row">
+    <section className="w-screen py-14 bg-background flex items-center min-md:justify-evenly max-xl:flex-col-reverse lg:flex-row ">
       <div className="flex items-center">
         <img
           src={logoImg}
-          className="w-[175px] p-5 max-lg:mt-10 min-lg:order-first "
+          className="size-[188px]  max-lg:mt-10 min-lg:order-first max-sm:size-[139px] max-sm:p-0"
         ></img>
-        <h1 className="text-dark-text font-medium text-[50px]">ELEVEN Jr.</h1>
+        <h1 className="text-dark-text font-medium text-[70.4px] max-sm:text-[50px]">ELEVEN Jr.</h1>
       </div>
       <div>
-        <h1 className="text-dark-text text-5xl pb-5 font-bold w-[80vw] max-w-[440px] max-lg:text-[30px] r">
+        <h1 className="text-dark-text text-5xl pb-5 font-bold w-[88vw] max-w-[440px] max-lg:text-[32px]  max-sm:max-w-[390px] ">
           Nossa História
         </h1>
-        <h2 className="text-dark-text text-[20px] max-w-[440px] text-justify">
+        <h2 className="text-dark-text text-[24px] w-[88vw] max-w-[440px] text-justify max-sm:text-[16px] max-sm:max-w-[390px]">
           Somos uma Empresa Júnior (EJ) vinculada ao Movimento Empresa Júnior,
           que surgiu da aspiração dos alunos de Relações Internacionais da
           UEPB em colocar em prática a teoria aprendida no curso e oferecer
--- FrontEnd/src/components/Sections/MissionSection/MissionSection.tsx
+++ FrontEnd/src/components/Sections/MissionSection/MissionSection.tsx
@@ -2,13 +2,13 @@ import rocketIcon from "@/assets/images/icons/planet-icon.svg";
 
 export default function MissionSection() {
   return (
-    <section className="bg-dark-background w-screen flex items-center max-md:justify-center min-md:justify-evenly max-lg:flex-col p-15">
+    <section className="bg-dark-background w-screen flex items-center max-md:justify-center min-md:justify-evenly max-lg:flex-col py-14">
       <div className="">
-        <h1 className="text-primary-text text-[45px] pb-5 font-bold max-w-[655px] max-lg:max-w-[390px] max-lg:text-[32px]">
+        <h1 className="text-primary-text text-[45px] w-[88vw] pb-10 font-bold max-w-[655px] max-sm:max-w-[392px] max-lg:text-[32px] ">
           A nossa missão como empresa{" "}
           <span className="text-emphasis">júnior</span>
         </h1>
-        <p className="text-justify text-subtext text-[20px] max-w-[655px] max-lg:max-w-[390px]">
+        <p className="text-justify text-subtext text-[20px] w-[88vw] max-w-[655px] max-sm:max-w-[388px] max-sm:text-[16px]">
           Nossa missão é ser a ponte estratégica que conecta pequenas e médias
           empresas (PMEs) ao potencial ilimitado do mercado global, combinando
           inovação disruptiva e expertise em Relações Internacionais para
@@ -18,7 +18,7 @@ export default function MissionSection() {
           receitas e construir resiliência
         </p>
       </div>
-      <img src={rocketIcon} className="w-[400px] max-lg:mt-10" />
+      <img src={rocketIcon} className="w-[400px] max-lg:mt-10 max-md:size-[284px]" />
     </section>
   );
 }
